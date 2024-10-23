339a6af7a753084f930a1a40af49b62ac202622c
shay-mcdowell <mcdowellshayann@gmail.com>
2024-10-23 12:59:19 -0400

    added pic carousel w low quality images

--- panhel-survey/src/components/pages/AboutUs.js
+++ panhel-survey/src/components/pages/AboutUs.js
@@ -5,12 +5,19 @@ import juliaheadshot from './photos/juliaheadshot.jpg';
 import moriahheadshot from './photos/moriahheadshot.jpeg';
 import shayheadshot from './photos/shayheadshot.jpeg';
 import mayaheadshot from './photos/mayaheadshot.jpeg';
+import Carousel from './Carousel';
 
 
 const AboutUs = () => {
+    const images = [
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs37vBWynSG_6T3SOtlhRXKxkewmRBb0xTPA&s',
+        'https://villanovan.com/wp-content/uploads/2022/04/graydon-paul.jpeg',
+        'https://via.placeholder.com/800x400/5733ff/fff',
+      ];
     return (
         <div className = "AboutUs">
-            <h1>About Us</h1>
+            <h1>Smarter Matches, Stronger Sisterhood</h1>
+            <Carousel images={images} />
             <h2>Our Why</h2>
             <p>“Sororify” seeks to streamline the Villanova Panhellenic Recruitment process. At Villanova roughly 600 girls rush eight sororities in the first round of recruitment known as Sisterhood Round. The process of “matching” girls to each sorority’s existing members based on like qualities takes hours to ensure that potential members have a good experience. All of the tedious work done by hand still doesn’t guarantee the PNMs will be matched with their compatible counterparts in sororities.</p>
             <p>Our algorithm bridges the gap by ensuring the best possible match between PNMS and current chapter members the sorority that fits them the best.</p>
---
+++ panhel-survey/src/components/pages/Carousel.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import "./Carousel.css";
+
+const CarouselIndicators = ({ images, activeIndex, onClick }) => {
+  return (
+    <div className="carousel__indicators">
+      {images.map((_, index) => (
+        <span
+          key={index}
+          className={`carousel__indicator ${
+            index === activeIndex ? 'active' : ''
+          }`}
+          onClick={() => onClick(index)}
+        />
+      ))}
+    </div>
+  );
+};
+const Carousel = ({ images, interval = 3000 }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const nextSlide = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+  const prevSlide = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+  useEffect(() => {
+    const slideInterval = setInterval(nextSlide, interval);
+    return () => clearInterval(slideInterval); // Clear interval on component unmount
+  }, [activeIndex, interval]);
+  return (
+    <div className="carousel">
+      <button onClick={prevSlide} className="carousel__btn carousel__btn--prev">
+        &lt;
+      </button>
+      <img
+        src={images[activeIndex]}
+        alt={`Slide ${activeIndex}`}
+        className="carousel__img"
+      />
+      <button onClick={nextSlide} className="carousel__btn carousel__btn--next">
+        &gt;
+      </button>
+      <CarouselIndicators
+        images={images}
+        activeIndex={activeIndex}
+        onClick={setActiveIndex}
+      />
+    </div>
+  );
+};
+export default Carousel;
