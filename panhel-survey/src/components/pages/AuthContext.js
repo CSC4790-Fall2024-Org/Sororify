320b3b013f4d258d0788838a4e20fd0a1f142e34
Julia Foy <119273175+jfoy3@users.noreply.github.com>
2024-11-20 13:13:02 -0500

    uncommented out lines

--- panhel-survey/src/components/pages/AuthContext.js
+++ panhel-survey/src/components/pages/AuthContext.js
@@ -8,22 +8,22 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const navigate = useNavigate(); // Use useHistory hook
 
-  // useEffect(() => {
-  //   const authToken = localStorage.getItem('authToken');
-  //   //console.log('Auth token:', authToken); // Debugging line
-  //   if (authToken) {
-  //     // Fetch user data based on the token
-  //     fetchUserData(authToken).then(userData => {
-  //       console.log('Fetched user data:', userData); // Debugging line
-  //       if (userData && userData.role) {
-  //         console.log('User role:', userData.role); // Debugging line
-  //       } else {
-  //         console.log('User role not found'); // Debugging line
-  //       }
-  //       setUser(userData);
-  //     });
-  //   }
-  // }, []);
+  useEffect(() => {
+    const authToken = localStorage.getItem('authToken');
+    //console.log('Auth token:', authToken); // Debugging line
+    if (authToken) {
+      // Fetch user data based on the token
+      fetchUserData(authToken).then(userData => {
+        console.log('Fetched user data:', userData); // Debugging line
+        if (userData && userData.role) {
+          console.log('User role:', userData.role); // Debugging line
+        } else {
+          console.log('User role not found'); // Debugging line
+        }
+        setUser(userData);
+      });
+    }
+  }, []);
 
   const signIn = (userData, token) => {
     //console.log('Setting auth token:', token); 
@@ -50,17 +50,17 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
-// const fetchUserData = async (token) => {
-//     try {
-//       const response = await axios.get('http://localhost:8000/api/auth/user/', {
-//         headers: {
-//           Authorization: `Bearer ${token}`
-//         }
-//       });
-//       console.log('Fetched user data:', response.data);
-//       return response.data; // Assuming the response data contains user information
-//     } catch (error) {
-//       console.error('There was an error fetching user data!', error);
-//       return null;
-//     }
-//   };
+const fetchUserData = async (token) => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/auth/user/', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      console.log('Fetched user data:', response.data);
+      return response.data; // Assuming the response data contains user information
+    } catch (error) {
+      console.error('There was an error fetching user data!', error);
+      return null;
+    }
+  };
