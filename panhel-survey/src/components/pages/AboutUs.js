0fab754f3c02d4708717f807c11a356a86caf8ee
amithasound <amithasound@gmail.com>
2024-12-04 09:31:51 -0500

    pic change

--- panhel-survey/src/components/pages/AboutUs.js
+++ panhel-survey/src/components/pages/AboutUs.js
@@ -12,6 +12,7 @@ import girl from './photos/girl.png';
 import kd from './photos/kd.png';
 import red from './photos/red.png';
 import kappamain from './photos/kappamain.png';
+import kappamain2 from './photos/kappamain2.png';
 import aphichurch from './photos/aphichurch.png';
 import dgcampus from './photos/DGcampus.png';
 
@@ -32,7 +33,8 @@ const AboutUs = () => {
       ];
     return (
         <div className="container">
-            <img src={kappamain} alt="Kappa Main" className="about-us-image" />
+            <img src={kappamain2} alt="Kappa Main" className="about-us-image" />
+           
            <div className="text-and-images">
                 <p>“Sororify” seeks to streamline the Villanova Panhellenic Recruitment process. At Villanova roughly 600 girls rush eight sororities in the first round of recruitment known as Sisterhood Round. The process of “matching” girls to each sorority’s existing members based on like qualities takes hours to ensure that potential members have a good experience. All of the tedious work done by hand still doesn’t guarantee the PNMs will be matched with their compatible counterparts in sororities. Our algorithm bridges the gap by ensuring the best possible match between PNMS and current chapter members the sorority that fits them the best.</p>
                 <div className="image-container">
